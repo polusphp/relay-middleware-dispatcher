3d02422a4b83bd56fc43848c7476b9e5127082e3
Andreas Sundqvist <andreas@oak-valley.se>
2018-04-12 11:05:07 +0200

    Add support for middleware resolver

--- src/Dispatcher.php
+++ src/Dispatcher.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 namespace Polus\MiddlewareDispatcher\Relay;
 
+use Interop\Http\Factory\ResponseFactoryInterface;
 use Polus\MiddlewareDispatcher\AbstractDispatcher;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
@@ -10,12 +11,23 @@ use Relay\Relay;
 
 class Dispatcher extends AbstractDispatcher
 {
+    /**
+     * @var callable|null
+     */
+    private $relayResolver;
+
+    public function __construct(ResponseFactoryInterface $responseFactory, ?callable $relayResolver = null)
+    {
+        parent::__construct($responseFactory);
+        $this->relayResolver = $relayResolver;
+    }
+
     public function dispatch(ServerRequestInterface $request): ResponseInterface
     {
         $middlewares = $this->middlewares->toArray();
         $middlewares[] = new FallbackResponseMiddleware($this->responseFactory);
 
-        $relay = new Relay($middlewares);
+        $relay = new Relay($middlewares, $this->relayResolver);
         return  $relay->handle($request);
     }
 
